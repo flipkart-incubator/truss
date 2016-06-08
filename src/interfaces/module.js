5e125dbc8e9827f32acc51ae56fa497d6e3ef166
Shivani Tiwari <shivani.tiwari@flipkart.com>
2016-06-08 20:48:06 +0530

    Test and document truss (#2)
    * JIRA ID SPU-1477 Initial commit jsdoc documantation for index file
    
    * JIRA ID SPU-1477 jsdoc documentation

--- src/interfaces/module.js
+++ src/interfaces/module.js
@@ -2,11 +2,27 @@ import Utils from "../helpers/utils";
 import {moduleS} from "./store";
 import PubSub from "./pubsub";
 
-
+/**
+ * All the modules created by this framework will be extended by this Module.
+ * @module Module
+ */
 let Module = (function () {
 	let modulePrivateData = new WeakMap();
 
+	/**
+	 * @class
+	 * @extends {@link PubSub}
+	 */
 	class Module extends PubSub {
+		/**
+		 *
+		 * @param moduleName {string} the name of the module
+		 * @param uniqueId {string} the unique id of the module
+		 * @param path {string} the path of the module
+		 * @param lifeCycleFlags {lifeCycleFlags} the initial value of the lifecycle flags
+		 * @param instanceConfig the configuration of the module passed
+		 * @param instanceData It is the reference of module
+		 */
 		constructor(moduleName, uniqueId, path, lifeCycleFlags, instanceConfig, instanceData) {
 			super();
 
@@ -26,6 +42,10 @@ let Module = (function () {
 			});
 		}
 
+		/**
+		 * renders the template using placeholder
+		 * @param placeholderData : The placeholder data for creation of template
+		 */
 		render(placeholderData) {
 
 			const containerSelector = this.getUniqueId();
@@ -33,14 +53,26 @@ let Module = (function () {
 			document.querySelector(`#${containerSelector}`).innerHTML = this.template(placeholders);
 		};
 
+		/**
+		 * gets all the events of all types subscribed by the module
+		 * @returns {array} array of subscriptions
+		 */
 		getAllSubscriptions() {
 			return modulePrivateData.get(this).moduleSubscriptions;
 		};
 
+		/**
+		 * gets the unique id of the module
+		 * @returns {string}
+		 */
 		getUniqueId() {
 			return modulePrivateData.get(this).uniqueId;
 		};
 
+		/**
+		 * gets the unique id  of the parent element
+		 * @returns {string}
+		 */
 		getParentInstanceId() {
 			let path = this.path.split(".");
 
@@ -60,49 +92,88 @@ let Module = (function () {
 			}
 		};
 
+		/**
+		 *
+		 * @returns {string}
+		 */
 		getModuleContainer() {
 			return `#${this.getUniqueId()}`;
 		};
 
+		/**
+		 *
+		 * @returns {Module.moduleName}
+		 */
 		getModuleName() {
 			return this.moduleName;
 		};
 
+		/**
+		 *
+		 * @returns {Module.instanceConfig.placeholders}
+		 */
 		getInstanceConfig() {
 			return this.instanceConfig.placeholders;
 		};
 
+		/**
+		 *
+		 * @returns {string}
+		 */
 		getCSSSelector() {
 			return Utils.getCSSSelector();
 		};
 
+		/**
+		 * @todo destroy the module instance
+		 */
 		destroy() {
 
 		};
 
+		/**
+		 * subscribe to the event
+		 * @param subscription
+		 * @param [eventName = subscription.eventName] {string}
+		 */
 		subscribe(subscription, eventName = subscription.eventName) {
 			subscription.eventSubscriber = this.getModuleContainer();
 			modulePrivateData.get(this).moduleSubscriptions.push(subscription);
 			super.subscribe(subscription, eventName);
 		};
 
+		/**
+		 * calls {@link PubSub.publish}
+		 * @param eventName {string}
+		 * @param message {string}
+		 */
 		publish(eventName, message) {
 			super.publish(eventName, message);
 		};
 
+		/**
+		 *
+		 * calls {@link PubSub.unsubscribe}
+		 * @param eventName {string}
+		 * @param callback {function} the callback function to be unsubscribed
+		 */
 		unsubscribe(eventName, callback) {
 			super.unsubscribe(this.getModuleContainer(), eventName, callback);
 		};
 
 
+		/**
+		 * This creates a new <i> div </i> element with the id same as the unique ID of the module. If compiledHTML is not provided, dom element is created progressively else page string is created.
+		 * @param module module to be rendered
+		 * @param compiledHTML {"string"|""}
+		 * @returns {string|undefined}
+		 */
 		static createModuleArena(module, compiledHTML) {
-			// If compiledHTML is not provided, start creating dom element progressively.
 			if (typeof compiledHTML !== "string") {
 				document.querySelector(module.instanceConfig.container).innerHTML = `<div id="${module.getUniqueId()}"></div>`;
 				return;
 			}
 
-			// If compiledHTML is provided, create page string.
 			if (compiledHTML.trim() === "") {
 				compiledHTML = `<div id="${module.getUniqueId()}"></div>`;
 			} else {
